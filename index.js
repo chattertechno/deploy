eb24c1aef35bb2b5006cdb06d57e2978f0d2816a
chattertechno <reneekrom28@gmail.com>
2020-11-26 13:56:43 -0800

    first commit my guy

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+const http = require ('http');
+const path = require ('path');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
